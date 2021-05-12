5bc7afefd7317b101d4aecff0c6d41f8d127cf56
Lee_Muxian <lee0906926618@gmail.com>
2021-05-12 03:03:16 -0700

    add hw3.py

--- hw3.py
+++ hw3.py
@@ -15,34 +15,34 @@ mqttc = paho.Client()
 # TODO: revise host to your IP
 host = "192.168.43.211"
 topic = "Mbed"
-topic1 = "Mbed/Threshold"
-topic2 = "Mbed/angle_detected"
+topic1 = "Mbed/gesture_ID"
+topic2 = "Mbed/feature"
 
 # Callbacks
 def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
 
-num_tilt = 0
+num_feature = 0
 def on_message(mosq, obj, msg):
     global num_tilt
-    if len(str(msg.payload)) < 40:
-        num_tilt = 0
+    #print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+    if len(str(msg.payload)) >= 48:
         print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
         s.write(bytes("\r", 'UTF-8'))
         time.sleep(1)
         s.write(bytes("/mode_stop/run\r\n", 'UTF-8'))
         time.sleep(2)
-    elif len(str(msg.payload)) >= 40 :
-        if num_tilt == 9 :
+    elif len(str(msg.payload)) <= 35 :
+        if num_feature == 9 :
             print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
-            num_tilt = 0
+            num_feature = 0
             s.write(bytes("\r", 'UTF-8'))
             time.sleep(1)
             s.write(bytes("/mode_stop/run\r\n", 'UTF-8'))
             time.sleep(2)
         else :
             print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
-            num_tilt = num_tilt + 1
+            num_feature = num_feature + 1
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
@@ -62,8 +62,8 @@ mqttc.connect(host, port=1883, keepalive=60)
 mqttc.subscribe(topic1, 0)
 mqttc.subscribe(topic2, 0)
 
-s.write(bytes("/gesture_UI/run\r\n", 'UTF-8'))
-s.write(bytes("/angle_det/run\r\n", 'UTF-8'))
+s.write(bytes("/accele_cap/run\r\n", 'UTF-8'))
+
 
 # Publish messages from Python
 num = 0
